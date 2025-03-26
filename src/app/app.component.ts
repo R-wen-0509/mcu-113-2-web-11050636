b15e13d7e0cf6f4f7f712bc56058e23017742ca3
Howard <shih9305@gmail.com>
2025-03-26 17:55:27 +0800

    feat(product): 產品卡片樣式實作

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
+import { ProductCardComponent } from './product-card/product-card.component';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ProductCardComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'shopping-cart';
