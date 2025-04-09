381166e013e367e9e63c2749319ee99e61397385
Howard <shih9305@gmail.com>
2025-04-09 19:45:03 +0800

    feat(product): 建立產品資料模型

--- src/app/product-card-list/product-card-list.component.ts
+++ src/app/product-card-list/product-card-list.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+
 import { ProductCardComponent } from '../product-card/product-card.component';
+import { Product } from '../models/product';
 
 @Component({
   selector: 'app-product-card-list',
@@ -8,19 +10,18 @@ import { ProductCardComponent } from '../product-card/product-card.component';
   styleUrl: './product-card-list.component.scss',
 })
 export class ProductCardListComponent {
-  productName = '書籍 A';
-  author = '作者甲、作者乙、作者丙';
-  company = '博碩文化';
-
-  isShow = true;
-
-  photoUrl = 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img';
-
-  createDate = new Date('2025/4/9');
-
-  price = 10000;
+  product = new Product({
+    id: 1,
+    name: '書籍 A',
+    authors: '作者甲、作者乙、作者丙',
+    company: '博碩文化',
+    isShow: true,
+    photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?tex:img',
+    createDate: new Date('2025/4/9'),
+    price: 10000,
+  });
 
   onsetDisplay(isShow: boolean): void {
-    this.isShow = isShow;
+    this.product.isShow = isShow;
   }
 }
